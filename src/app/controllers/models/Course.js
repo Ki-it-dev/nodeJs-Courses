13a3b1a887138985dcefcbabcf35e3ee849c4df3
devkhoa <khoale548@gmail.com>
2021-10-10 21:24:47 +0700

    create Course

--- src/app/controllers/models/Course.js
+++ src/app/controllers/models/Course.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose')
+const slug = require('mongoose-slug-generator')
 const Schema = mongoose.Schema
 
+mongoose.plugin(slug);
+
 const Course = new Schema({
-    name: { type: String },
+    name: { type: String, require:true,},
     description: { type: String },
-    image: { type: String },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-    slug: { type: String },
-    videoID: { type: String },
+    image: { type: String, default:'https://cdn.fullstack.edu.vn/f8-learning/courses/6.png' },
+    slug: { type: String, slug: 'name', unique:true,},
+    videoID: { type: String, require:true,},
+    level: { type: String},
+},{
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course)
